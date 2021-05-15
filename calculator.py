2bf00c20be4b39c127b9bf5a024dcf011d4a9287
sagnikmitra <sagnikmitra123@gmail.com>
2021-05-15 09:15:14 +0530

    streamlit tutorials

---
+++ calculator.py
@@ -0,0 +1,55 @@
+import streamlit as st
+st.write("###  My First Calculator")
+first_number = st.number_input("Enter the First Number")
+operation = st.selectbox(
+    "Select Operation", ("Add", "Substract", "Multiply", "Divide", "Modulus", "Raised to the Power"))
+second_number = st.number_input("Enter the Second Number")
+output_type = st.radio(
+    "Select Output Type", ("Int", "Float"))
+st.write("### Output:")
+# Doing required operation regarding the selected option
+if output_type == "Float":
+    if operation == "Add":
+        result = (first_number+second_number)
+        st.write(f"# {first_number}+{second_number} = {result}")
+    elif operation == "Substract":
+        result = (first_number-second_number)
+        st.write(f"# {first_number}-{second_number} = {result}")
+    elif operation == "Multiply":
+        result = (first_number*second_number)
+        st.write(f"# {first_number}*{second_number} = {result}")
+    elif operation == "Divide":
+        result = (first_number/second_number)
+        st.write(f"# {first_number}/{second_number} = {result}")
+    elif operation == "Modulus":
+        result = (first_number % second_number)
+        st.write(f"# {first_number}%{second_number} = {result}")
+    elif operation == "Raised to the Power":
+        result = (first_number**second_number)
+        st.write(f"# {first_number}^{second_number} = {result}")
+    else:
+        st.write("Choose a valid operation")
+
+if output_type == "Int":
+    first_number = int(first_number)
+    second_number = int(second_number)
+    if operation == "Add":
+        result = int(first_number+second_number)
+        st.write(f"# {first_number}+{second_number} = {result}")
+    elif operation == "Substract":
+        result = int(first_number-second_number)
+        st.write(f"# {first_number}-{second_number} = {result}")
+    elif operation == "Multiply":
+        result = int(first_number*second_number)
+        st.write(f"# {first_number}*{second_number} = {result}")
+    elif operation == "Divide":
+        result = int(first_number/second_number)
+        st.write(f"# {first_number}/{second_number} = {result}")
+    elif operation == "Modulus":
+        result = int(first_number % second_number)
+        st.write(f"# {first_number}%{second_number} = {result}")
+    elif operation == "Raised to the Power":
+        result = int(first_number**second_number)
+        st.write(f"# {first_number}^{second_number} = {result}")
+    else:
+        st.write("Choose a valid operation")
---
+++ markdown.py
@@ -0,0 +1,16 @@
+import streamlit as st
+st.write("""
+# This is heading H1
+## This is heading H2
+### This is heading H3
+#### This is heading H4
+##### This is heading H5
+###### This is heading H6  
+
+&nbsp;
+&nbsp;
+
++ It also allow you 
+- to add bullets in
+* different formats
+""")
